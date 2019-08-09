109fd5d130ce419e4dbcdb74a1b96beab962efa9
abautista <daddy_brito@hotmail.com>
2019-08-09 17:05:03 -0600

    se agrego EF

--- WebApplication4/WebApplication4/ESB-autos.asmx.cs
+++ WebApplication4/WebApplication4/ESB-autos.asmx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using System.Web;
 using System.Web.Services;
+using WebApplication4.Model;
 
 namespace WebApplication4
 {
@@ -21,6 +23,65 @@ namespace WebApplication4
         public string HelloWorld()
         {
             return "Hola a todos";
+        }
+
+        [WebMethod]
+        public bool AgregarPiloto()
+        {
+            try
+            {
+                using (Ubr_201212487Entities db = new Ubr_201212487Entities())
+                {
+                    Piloto pl = new Piloto();
+                    pl.id = Convert.ToInt32(getUtimoPiloto());
+                    pl.Nombre = "Piloto " + pl.id;
+                    pl.Automovil = "Automovil " + pl.id;
+
+                    db.Piloto.Add(pl);
+                    db.SaveChanges();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private string getUtimoPiloto()
+        {
+            string ultimoCOdigo = "";
+            
+            try
+            {
+                using (TransactionScope trn = new TransactionScope(TransactionScopeOption.Suppress))
+                {
+                    using (Ubr_201212487Entities db = new Ubr_201212487Entities())
+                    {
+                        db.Piloto.Add(new Piloto());
+
+                        Piloto pl = db.Piloto.ToList().Last();
+
+                        if (pl.Equals(null))
+                        {
+                            ultimoCOdigo = "1";
+                        }else
+                        {
+                            ultimoCOdigo = (pl.id + 1).ToString();
+                        }
+
+                        //int intCurrentId = Convert.ToInt32(db.Database.SqlQuery<decimal>("SELECT IDENT_CURRENT ('Col_ATM')", new object[0]).FirstOrDefault());
+
+                        //ultimoCOdigo = (intCurrentId + 1).ToString();
+                    }
+                }
+            }
+            catch
+            {
+                ultimoCOdigo = "1";
+            }
+            return ultimoCOdigo;
         }
 
         [WebMethod]
